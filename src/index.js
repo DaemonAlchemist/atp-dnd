f7fc853f4a74032d71cbfdc9378e2704cb0c2266
Andrea <andrea@wittrock.us>
2017-11-03 01:01:27 -0500

    Add dragSource function

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@
 
 import {DragSource, DropTarget} from 'react-dnd';
 
+export const dragSource = type => DragSource(
+    type,
+    {
+        beginDrag: (props, monitor, component) => ({id: props.id}),
+        endDrag: (props, monitor) => {
+            const result = monitor.getDropResult();
+            if(result) {
+                props.onMove(result);
+            }
+        }
+    },
+    (connect, monitor) => ({
+        dragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+    })
+);

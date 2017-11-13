0280fce0632b2511fb22715a4bb01ef3e0110dc7
Andrea <andrea@wittrock.us>
2017-11-12 23:20:49 -0600

    Remove hierarchicalDropTarget

--- src/index.js
+++ src/index.js
@@ -10,50 +10,6 @@ import Draggable from "./containers/drag-source";
 import DropTarget, {dropTargetContext} from "./containers/drop-target";
 import {addContext} from 'atp-react-context';
 
-const hierarchicalDropTarget = ({type, action, name, accepts}) => DropTargetBase(
-    props => o(accepts(props)).keys().concat(typeof type === 'function' ? type(props) : type),
-    {
-        drop: (props, monitor) => {
-            const item = monitor.getItem();
-            const result = {
-                action: typeof action === 'function' ? action(item) : action,
-                sourceType: item.type,
-                sourceId: item.id,
-                targetId: props.id
-            };
-
-            if(props.onReceiveDrop) {
-                notEmpty(result).then(props.onReceiveDrop);
-            }
-
-            return result;
-        },
-        canDrop: (props, monitor) => {
-            const thisType = typeof type === 'function' ? type(props) : type;
-            const options = o(accepts(props)).merge({
-                [thisType]: item => {
-                    const sourceId = item.id;
-                    const restrictedNodes = (props.parents || []).concat(props.id);
-                    const canDrop = !restrictedNodes.includes(sourceId);
-                    return canDrop;
-                }
-            }).raw;
-            const item = monitor.getItem();
-            if(typeof options[item.type] !== 'undefined') {
-                return options[item.type](item);
-            } else {
-                console.log("Unsupported drop type: " + item.type);
-                return false;
-            };
-        }
-    },
-    (connect, monitor) => ({
-        [name + 'DropTarget']: connect.dropTarget(),
-        [name + 'CanDrop']: monitor.canDrop(),
-        [name + 'IsOver']: monitor.isOver(),
-    })
-);
-
 const Active = addContext(dropTargetContext)(props =>
     props.dropTargetIsOver ? React.Children.map(props.children, child => child) : null
 );
@@ -70,4 +26,4 @@ const CannotDrop = addContext(dropTargetContext)(props =>
     !props.dropTargetCanDrop ? React.Children.map(props.children, child => child) : null
 );
 
-export {hierarchicalDropTarget, Draggable, DropTarget, Active, Inactive, CanDrop, CannotDrop};
+export {Draggable, DropTarget, Active, Inactive, CanDrop, CannotDrop};
